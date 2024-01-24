712d0d1367c6ebecbfd8be3e94b3f7c63257fcbb
peiciqiu <peiciqiu@seas.upenn.edu>
2024-01-24 09:14:55 -0500

    finish and test 95

--- Server/routes/places-routes.js
+++ Server/routes/places-routes.js
@@ -1,38 +1,12 @@
 const express = require('express');
+
+const placesControllers = require('../controllers/places-controllers');
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous skyscrapers in the world!',
-        location: {
-            lat: 40.75,
-            lng: -73.99
-        },
-        address: '20 W 34th St., New York, NY 10001',
-        creator: 'u1'
-    }
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
-
-    res.json({place}); // === {place:place}
-});
-
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    });
-
-    res.json({ place });
-});
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
 module.exports = router;
 
